c8c19e8e57b26ef16a5ee07c0d2ece03c1b5e6aa
mistmago <gollenamikestephen@gmail.com>
2026-05-03 18:52:52 +0800

    refactor: modify web.php for lpage

--- routes/web.php
+++ routes/web.php
@@ -3,43 +3,40 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\DashboardController;
 
-// dashboard pages
+
 Route::get('/', function () {
+    return view('landingpage');
+});
+
+Route::get('/dashboard', function () {
     return view('pages.dashboard.ecommerce', ['title' => 'E-commerce Dashboard']);
 })->name('dashboard');
 
-// calender pages
 Route::get('/calendar', function () {
     return view('pages.calender', ['title' => 'Calendar']);
 })->name('calendar');
 
-// profile pages
 Route::get('/profile', function () {
     return view('pages.profile', ['title' => 'Profile']);
 })->name('profile');
 
-// form pages
 Route::get('/form-elements', function () {
     return view('pages.form.form-elements', ['title' => 'Form Elements']);
 })->name('form-elements');
 
-// tables pages
 Route::get('/basic-tables', function () {
     return view('pages.tables.basic-tables', ['title' => 'Basic Tables']);
 })->name('basic-tables');
 
 // pages
-
 Route::get('/blank', function () {
     return view('pages.blank', ['title' => 'Blank']);
 })->name('blank');
 
-// error pages
 Route::get('/error-404', function () {
     return view('pages.errors.error-404', ['title' => 'Error 404']);
 })->name('error-404');
 
-// chart pages
 Route::get('/line-chart', function () {
     return view('pages.chart.line-chart', ['title' => 'Line Chart']);
 })->name('line-chart');
@@ -49,7 +46,6 @@ Route::get('/bar-chart', function () {
 })->name('bar-chart');
 
 
-// authentication pages
 Route::get('/signin', function () {
     return view('pages.auth.signin', ['title' => 'Sign In']);
 })->name('signin');
@@ -58,7 +54,6 @@ Route::get('/signup', function () {
     return view('pages.auth.signup', ['title' => 'Sign Up']);
 })->name('signup');
 
-// ui elements pages
 Route::get('/alerts', function () {
     return view('pages.ui-elements.alerts', ['title' => 'Alerts']);
 })->name('alerts');
@@ -82,25 +77,3 @@ Route::get('/image', function () {
 Route::get('/videos', function () {
     return view('pages.ui-elements.videos', ['title' => 'Videos']);
 })->name('videos');
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
