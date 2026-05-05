e3ca77aef7776ad5381cbad8004066909b03dce3
mistmago <gollenamikestephen@gmail.com>
2026-05-05 23:50:46 +0800

    style: change icon

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,6 +7,7 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
     <title>{{ $title ?? 'Dashboard' }} | TailAdmin - Laravel Tailwind CSS Admin Dashboard Template</title>
+    <link rel="shortcut icon" href="{{ asset('assets/images/clogo.svg') }}" type="image/x-icon">
 
     <!-- Scripts -->
     @vite(['resources/css/app.css', 'resources/js/app.js'])
