ed1145e61ff6aa82aa377bea30832148e4325f7b
MISTMAGO <gollenamikestephen@gmail.com>
2026-07-11 12:32:30 +0800

    mod: unified admin and staff dashboard

--- resources/views/pages/dashboard/dashboard.blade.php
+++ resources/views/pages/dashboard/dashboard.blade.php
@@ -41,7 +41,7 @@
         </div>
     @endrole
 
-    @role('staff')
+    @unlessrole('client')
         <div class="grid grid-cols-12 gap-6">
             <div class="col-span-12">
                 <x-dashboard.staff-summary :rooms="$rooms" :allBookings="$allBookings" :totalRooms="$totalRooms" :availableRooms="$availableRooms" />
@@ -54,32 +54,10 @@
             <x-dashboard.pending-approvals :pendingBookings="$pendingBookings" />
 
             <div class="col-span-12 md:col-span-6 space-y-6">
-                <x-dashboard.client-quick-actions />
+                {{-- <x-dashboard.client-quick-actions /> --}}
                 <x-dashboard.client-recent-bookings :bookings="$bookings" />
             </div>
         </div>
-    @endrole
+    @endunlessrole
 
-    @role('admin')
-        <div class="grid grid-cols-12 gap-4 md:gap-6">
-            <div class="col-span-12 space-y-6 xl:col-span-7">
-            <x-ecommerce.ecommerce-metrics />
-            </div>
-            <div class="col-span-12 xl:col-span-5">
-                <x-ecommerce.monthly-target />
-            </div>
-
-            <div class="col-span-12">
-            <x-ecommerce.statistics-chart />
-            </div>
-
-            <div class="col-span-12 xl:col-span-5">
-            {{-- <x-ecommerce.customer-demographic /> --}}
-            </div>
-
-            <div class="col-span-12 xl:col-span-7">
-            {{-- <x-ecommerce.recent-orders /> --}}
-            </div>
-        </div>
-    @endrole
 @endsection
